c16a44b2ec09fa9fbe9a35480a0122f53b0b17dd
GibsoN92 <52374403+GibsoN92@users.noreply.github.com>
2024-06-07 18:03:25 +0200

    v1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,22 @@
-function App() {
+import React from "react";
+import Profile from "./Profile/Profile";
+import user from "./../data/user.json";
+import Statistics from "./Statistics/Statistics";
+import data from "./../data/data.json";
+import FriendList from "./FriendList/FriendList";
+import friends from "./../data/friends.json";
+import TransactionHistory from "./TransactionHistory/TransactionHistory";
+import transactions from "./../data/transactions.json";
+
+const App = () => {
   return (
     <>
-      <h1>React Homework Template (Vite)</h1>
+      <Profile user={user} />
+      <Statistics title="Upload stats" data={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
-}
+};
 
 export default App;
